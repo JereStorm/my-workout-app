7427d5e9743ccb7ba6e9136dcb3b8dd882d6836f
Jeremias Storm <jerestorm@gmail.com>
2025-05-19 17:52:07 -0300

    [DEPLOY]: config gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), ghPages()],
+  base: "my-workout-app",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
