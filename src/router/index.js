68cc7f7ddd18f9e705bf73fd25f94a14bb358334
Jeremias Storm <jerestorm@gmail.com>
2025-05-19 20:03:25 -0300

    [FIX]: Se agrega prefijo en todas las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -11,26 +11,26 @@ import ProfilePage from "@/components/ProfilePage.vue";
 import FormRoutine from "@/components/FormRoutine.vue";
 
 const routes = [
-    { path: "/register", component: RegisterPage },
-    { path: "/login", component: LoginPage },
+    { path: "my-workout-app/register", component: RegisterPage },
+    { path: "my-workout-app/login", component: LoginPage },
     {
-        path: "/dashboard",
+        path: "my-workout-app/dashboard",
         component: DashboardPage,
         children: [
             {
                 path: "",
-                redirect: "/dashboard/my-workouts", // Redirige /dashboard a /dashboard/my-workouts
+                redirect: "my-workout-app/dashboard/my-workouts", // Redirige /dashboard a /dashboard/my-workouts
             },
             {
-                path: '/dashboard/my-workouts',
+                path: 'my-workout-app/dashboard/my-workouts',
                 component: MyWorkoutsPage
             },
             {
-                path: '/dashboard/my-profile',
+                path: 'my-workout-app/dashboard/my-profile',
                 component: ProfilePage
             },
             {
-                path: '/dashboard/form-routine',
+                path: 'my-workout-app/dashboard/form-routine',
                 component: FormRoutine
             },
 
@@ -38,8 +38,8 @@ const routes = [
         meta: { requiresAuth: true },
     },
 
-    { path: "/unregistred", component: UnregistredPage },
-    { path: "/:pathMatch(.*)*", component: UnregistredPage }, // Captura rutas no definidas
+    { path: "my-workout-app/unregistred", component: UnregistredPage },
+    { path: "my-workout-app/:pathMatch(.*)*", component: UnregistredPage }, // Captura rutas no definidas
 ];
 
 const router = createRouter({
